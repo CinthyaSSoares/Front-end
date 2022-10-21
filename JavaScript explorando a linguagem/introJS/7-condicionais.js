035663afda2ef949a9d209145c27de55e5c0bb5c
Cinthya <cinthyadasilvasoares@outlook.com>
2022-10-21 09:50:02 -0300

    adicionando aula 7 if e else

---
+++ JavaScript explorando a linguagem/introJS/7-condicionais.js
@@ -0,0 +1,21 @@
+console.log(`Trabalhando com condicionais`);
+
+const listaDeDestinos = new Array(
+    `Salvador`,
+    `São Paulo`,
+    `Rio de Janeiro`
+);
+
+const idadeComprador = 16;
+console.log("Destinos possíveis:");
+console.log(listaDeDestinos);
+
+if(idadeComprador >=18) {
+    console.log("Comprador maior de idade");
+    listaDeDestinos.splice(1,1); //removendo item
+} else{
+    console.log("Não é maior de idade, não posso vender");
+}
+
+console.log(listaDeDestinos);
+
