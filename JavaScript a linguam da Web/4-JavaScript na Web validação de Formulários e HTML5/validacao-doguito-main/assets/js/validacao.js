8d5cb40b3d607bdebeb294df810b6fbd7c6c21be
Cinthya <cinthyadasilvasoares@outlook.com>
2022-11-08 11:01:29 -0300

    Tipos de erro/ criando logica para validar os tipos de erro para preencher o formulario em nome/email e data

--- JavaScript a linguam da Web/4-JavaScript na Web validação de Formulários e HTML5/validacao-doguito-main/assets/js/validacao.js
+++ JavaScript a linguam da Web/4-JavaScript na Web validação de Formulários e HTML5/validacao-doguito-main/assets/js/validacao.js
@@ -2,12 +2,57 @@ export function valida(input) {
     const tipoDeInput = input.dataset.tipo;
 
     if(validadores[tipoDeInput]) {
-        validadores[tipoDeInput](input)
+        validadores[tipoDeInput](input);
+    }
+
+    if(input.validity.valid) {
+        input.parentElement.classList.remove('input-container--invalido')
+        input.parentElement.querySelector('.input-mensagem-erro').innerHTML = ''
+    } else {
+        input.parentElement.classList.add('input-container--invalido')
+        input.parentElement.querySelector('.input-mensagem-erro').innerHTML = mostraMensagemDeErro(tipoDeInput, input)
     }
 }
 
+const tiposDeErro = [
+    'valueMissing',
+    'typeMismath',
+    'patternMismatch',
+    'customError'
+]
+
+const mensagensDeErro = {
+    nome: {
+        valueMissing: 'O campo nome não pode estar vazio.'
+    },
+    email: {
+        valueMissing: 'O campo de email não pode estar vazio.',
+        typeMismath: 'O email digitado não é válido.'
+    },
+    senha: {
+        valueMissing: 'O campo de senha não pode estar vazio.',
+        patternMismatch: 'A senha deve conter entre 6 a 12 caracteres, e no mínimo: uma letra maiúscula, um número e não deve conter símbolos.',
+    },
+    dataNascimento: {
+        valueMissing: 'O campo de data de nascimento não pode estar vazio.',
+        customError: 'Você deve ser maior que 18 anos para se cadastrar.'
+    }
+
+}
+
 const validadores = {
-    dataNascimento:input => validaDataNascimento(input);
+    dataNascimento:input => validaDataNascimento(input)
+}
+
+function mostraMensagemDeErro(tipoDeInput, input) {
+let mensagem = ''
+tiposDeErro.forEach(erro => {
+    if(input.validity[erro]) {
+        mensagem = mensagensDeErro[tipoDeInput][erro]
+    }
+})
+
+    return mensagem
 }
 
 function validaDataNascimento(input) {
