b1eec28d1b0a152454413e2087c9f80cea8bbef3
Cinthya Da Silva Soares <97134413+CinthyaSSoares@users.noreply.github.com>
2022-12-08 11:51:40 -0300

    Atualizando

---
+++ Java e Orientacao a Objetos/1-Entender a linguagem Java e orientação a objetos/a-Java JRE e JDK compile e execute o seu programa/a-Java JRE e JDK compile e execute o seu programa/sintaxeVariaveisEFluxos/src/sintaxeVariaveisEFluxos/TestaValores.java
@@ -0,0 +1,28 @@
+package sintaxeVariaveisEFluxos;
+
+public class TestaValores {
+	
+	// auto-complete do public static "main" + "ctrl+espaço" e clicar no primeiro
+	public static void main(String[] args) {
+		int primeiro = 5;
+		int segundo = 7;
+		
+		System.out.println(segundo);
+		
+		segundo = primeiro;
+		System.out.println(segundo);
+		
+		
+
+		String saudacao = "Olá, meu nome é ";
+		String nome = "Cinthya";
+		String continuacao = ", minha idade é ";
+		int idade = 24;
+		System.out.println(saudacao+nome+continuacao+idade);
+		
+		
+		
+		
+	}
+
+}
